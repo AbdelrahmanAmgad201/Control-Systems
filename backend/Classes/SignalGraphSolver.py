c07e1d8135862112655386c1321a649e90511fd3
Ali <alimk27645@gmail.com>
2025-04-14 21:28:55 +0200

    solving signal flow diagram and gettting transfer function

--- backend/Classes/SignalGraphSolver.py
+++ backend/Classes/SignalGraphSolver.py
@@ -2,31 +2,34 @@ from dataclasses import dataclass
 from typing import List, Set, Tuple, Dict
 from Graph import Graph
 from Loop import Loop
+from Path import Path
 import itertools
 
 class SignalGraphSolver:
-    def __init__(self, G: List[Loop]):
-        self.all_loops = G
+    def __init__(self, loops: List[Loop], paths:List[Path]):
+        self.all_loops = loops
         self.all_pairof_loops = []  # Each element is a list of combinations of loops of a certain order
-        self.gains = []
-
+     
+        self.paths=paths  
         self.all_pairof_loops.append([[loop] for loop in self.all_loops])
-        self.gains.append([loop.gain for loop in self.all_loops])
+        
     
-    def get_non_touching_loops(self):
-        max_order = len(self.all_loops)
+    def get_non_touching_loops(self,all_loops):
+        gains=[]
+        gains.append([loop.gain for loop in all_loops])
+        max_order = len(all_loops)
         for order in range(2, max_order + 1):
             current_combinations = []
             current_gains = []
             if order == 2:
-                for combo in itertools.combinations(self.all_loops, order):
+                for combo in itertools.combinations(all_loops, order):
                     if self.are_mutually_non_touching(combo):
                         current_combinations.append(list(combo))
                         current_gains.append(self.combo_gain(combo))
             else:
                 prev_order_index = order - 2 
                 for prev_combo in self.all_pairof_loops[prev_order_index]:
-                    for loop in self.all_loops:
+                    for loop in all_loops:
                         if loop not in prev_combo:
                             new_combo = prev_combo.copy()
                             new_combo.append(loop)
@@ -38,11 +41,11 @@ class SignalGraphSolver:
             
             if current_combinations:
                 self.all_pairof_loops.append(current_combinations)
-                self.gains.append(current_gains)
+                gains.append(current_gains)
             else:
                 break
                 
-        return self.gains, self.all_pairof_loops
+        return gains.copy(), self.all_pairof_loops.copy
     
     def are_mutually_non_touching(self, loops: List[Loop]) -> bool:
         for i in range(len(loops)):
@@ -56,6 +59,48 @@ class SignalGraphSolver:
         for loop in loops:
             gain *= loop.gain
         return gain
+    def get_deltas(self):
+        deltas = []
+        for path in self.paths:
+            # Find loops that don't touch this path
+            non_touching_loops = []
+            for loop in self.all_loops:
+                touches_path = False
+                for node in loop.nodes:
+                    if node in path.path:
+                        touches_path = True
+                        break
+                if not touches_path:
+                    non_touching_loops.append(loop)
+            
+            # Create a temporary solver with just these loops
+            temp_solver = SignalGraphSolver(non_touching_loops, [])
+            delta = temp_solver.get_delta()
+            deltas.append(delta)
+        
+        return deltas
+    
+
+
+
+    def get_delta(self):
+        gains,loops=self.get_non_touching_loops(self.all_loops)
+        res=0
+        sign=-1
+        for i in gains:
+            sign*=-1
+            for j in i: 
+                res+=sign*j 
+        return 1-res
+    
+    def solve(self):
+        res=0
+        deltas=self.get_deltas()
+        delta=self.get_delta()
+        for i in range(len(self.paths)):
+            res+=self.paths[i].gain*deltas[i]
+        res/=delta 
+        return res
 
 # Test code
 loop1 = Loop()
@@ -78,19 +123,18 @@ loop5 = Loop()
 loop5.nodes = ['K', 'P']
 loop5.gain = 3.7
 
+path1=Path(['A','B','C','D'],7)
+path2=Path(['P','A','T','R'],7)
 test_loops = []
 test_loops.append(loop1)
 test_loops.append(loop2)
 test_loops.append(loop3)
 test_loops.append(loop4)
 test_loops.append(loop5)
+test_paths=[]
+test_paths.append(path1)
+test_paths.append(path2)
+solver = SignalGraphSolver(test_loops,test_paths)
 
-solver = SignalGraphSolver(test_loops)
-gains, loops = solver.get_non_touching_loops()
-print("Gains:", gains)
-print("\nNon-touching loop combinations:")
-for order, combinations in enumerate(loops, 1):
-    print(f"\nOrder {order}:")
-    for combo_index, combo in enumerate(combinations):
-        nodes_list = [loop.nodes for loop in combo]
-        print(f"  Combination {combo_index+1}: {nodes_list}, Gain: {gains[order-1][combo_index]}")
+print(solver.solve())
+
