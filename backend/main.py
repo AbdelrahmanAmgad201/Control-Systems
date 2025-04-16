8a132fc9d3d8f58cc52ed7448d2391fe450796ef
mohamedelsayed0001 <mohamedelsayyed234@gmail.com>
2025-04-16 14:06:24 +0200

    plz git dont eat the code again

--- backend/main.py
+++ backend/main.py
@@ -2,8 +2,10 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS  # <--- import CORS
 from Classes.Graph import Graph
 from Classes.SignalGraphSolver import SignalGraphSolver
+from Stability.Routh_criteria import routh_criteria
 import traceback
 from mssgDTOs.InputDTO import InputDTO
+import numpy as np
 
 app = Flask(__name__)
 CORS(app)  # <--- allow all origins (for development)
@@ -53,6 +55,32 @@ def solve_signal_flow_graph():
             "success": False,
             "error": str(e)
         }), 400
-
+@app.route("/solve_characteristic_equation", methods=["POST"])
+def solve_characteristic_equation():
+    try:
+        
+        data = request.get_json()
+        coeffs = data['coeffs']
+        order = data['order']
+        result = routh_criteria(coeffs, order)
+        
+        def convert_complex(obj):
+            if isinstance(obj, (complex, np.complex128, np.complex64)):
+                return {"real": obj.real, "imag": obj.imag}
+            elif isinstance(obj, np.ndarray):  # Convert NumPy arrays to lists
+                return obj.tolist()
+            elif isinstance(obj, list):  # Recursively process lists
+                return [convert_complex(item) for item in obj]
+            elif isinstance(obj, dict):  # Recursively process dictionaries
+                return {key: convert_complex(value) for key, value in obj.items()}
+            else:
+                return obj 
+        
+        result = convert_complex(result)
+        return jsonify(result)
+    
+    except Exception as e:
+        print(e)
+        return jsonify({"error": str(e)}), 500
 if __name__ == '__main__':
     app.run(debug=True)
